f31bfb392238ba3f04755192ae196174d4b9918c
Ben Palmer <ben33palmer@gmail.com>
2014-08-07 21:08:55 +0100

    Improved handling of '?' in GetData script

--- GetData.R
+++ GetData.R
@@ -10,16 +10,18 @@
 
 require(data.table) #-- Enable use of fread function
 
+#,colClasses = c("character","character","character","character","character","character","character","character","character")
+
 #========================================================================================
 # LOAD MAIN DATASET
 #========================================================================================
-mydata <- fread("C:/Users/Ben/STUDY/Exploratory.Data.Analysis/Assignment.01/household_power_consumption.txt",stringsAsFactors = FALSE,header = TRUE,sep=";")
+mydata <- fread("C:/Users/Ben/STUDY/Exploratory.Data.Analysis/Assignment.01/household_power_consumption.txt",colClasses = c("character","character","character","character","character","character","character","character","character") ,stringsAsFactors = FALSE,header = TRUE,sep=";",na.strings = c("?"))
 
 #========================================================================================
 # SUBSET DATA 
 #========================================================================================
 power.con <- subset(mydata,Date == "1/2/2007"|Date == "2/2/2007")
-
+#summary(power.con)
 
 #========================================================================================
 # FREE MEMORY BY DROPPING MAIN DATASET
@@ -36,42 +38,7 @@ rm(mydata)
 dFormat <- "%d/%m/%Y %H:%M:%S"
 power.con$Date <-as.POSIXct(paste(power.con$Date,power.con$Time), format=dFormat)
 
-#----- CONVERT '?' TO NA -------#
 
-# Global_active_power
-if (length(which(power.con$Global_active_power == "?")) > 0) {
-  power.con$Global_active_power <- replace(power.con$Global_active_power,which(power.con$Global_active_power == "?"), NA) 
-}
-
-# Global_reactive_power
-if (length(which(power.con$Global_reactive_power == "?")) > 0) {
-  power.con$Global_reactive_power <- replace(power.con$Global_reactive_power,which(power.con$Global_reactive_power == "?"), NA)
-}
-
-# Voltage
-if (length(which(power.con$Voltage == "?")) > 0) {
-  power.con$Voltage <- replace(power.con$Voltage,which(power.con$Voltage == "?"), NA)
-}
-
-# Global_intensity
-if (length(which(power.con$Global_intensity == "?")) > 0) {
-  power.con$Global_intensity <- replace(power.con$Global_intensity,which(power.con$Global_intensity == "?"), NA)
-}
-
-# Sub_metering_1
-if (length(which(power.con$Sub_metering_1 == "?")) > 0) {
-  power.con$Sub_metering_1 <- replace(power.con$Sub_metering_1,which(power.con$Sub_metering_1 == "?"), NA)
-}
-
-# Sub_metering_2
-if (length(which(power.con$Sub_metering_2 == "?")) > 0) {
-  power.con$Sub_metering_2 <- replace(power.con$Sub_metering_2,which(power.con$Sub_metering_2 == "?"), NA)
-}
-
-# Sub_metering_3
-if (length(which(power.con$Sub_metering_3 == "?")) > 0) {
-  power.con$Sub_metering_3 <- replace(power.con$Sub_metering_3,which(power.con$Sub_metering_3 == "?"), NA)
-}
 
 #----- CONVERT NUMERIC FIELDS -----#
 power.con$Global_active_power <- as.numeric(power.con$Global_active_power)
@@ -81,3 +48,4 @@ power.con$Sub_metering_1 <- as.numeric(power.con$Sub_metering_1)
 power.con$Sub_metering_2 <- as.numeric(power.con$Sub_metering_2)
 power.con$Sub_metering_3 <- as.numeric(power.con$Sub_metering_3)
 #========================================================================================
+
